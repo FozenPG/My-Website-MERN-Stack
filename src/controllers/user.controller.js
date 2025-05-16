0e0fd53771e8c69f1479f4671bdc5e1f3598c481
FozenPG <fozenpg@gmail.com>
2025-05-16 10:33:16 -0300

    aula 10

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const userService = require("../services/user.service")
+const mongoose = require("mongoose")
 
 const create = async (req, res) => {
     const { name, username, email, password, avatar, background } = req.body
@@ -7,7 +8,7 @@ const create = async (req, res) => {
         res.status(400).json("Submit all fields to proceed!")
     }
 
-    const user = await userService.create(req.body)
+    const user = await userService.createService(req.body)
 
     if (!user) {
         return res.status(400).send({Message: "Error creating user"})
@@ -26,4 +27,34 @@ const create = async (req, res) => {
     })
 }
 
-module.exports = { create }
+const findAll = async (req, res) => {
+
+    const users = await userService.findAllService()
+
+    if (users === 0) {
+        return res.status(400).send({message: "ther are no users in the system"})
+    }
+
+    res.send(users)
+}
+
+const findById = async (req, res) => {
+    const id = req.params.id
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).send({message: "invalid Id"})
+    }
+
+    const user = await userService.findByIdService(id)
+
+    if (!user) {
+        return res.status(400).send({message: "this users doesn't exists"})
+    }
+
+    res.send(user)
+}
+module.exports = { 
+    create,
+    findAll,
+    findById
+}
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,5 +2,18 @@ const route = require("express").Router()
 const userController = require("../controllers/user.controller")
 
 route.post("/", userController.create)
+route.get("/", userController.findAll)
+route.get("/:id", userController.findById)
 
 module.exports = route
+
+/*
+{
+  "name": "jose",
+  "username": "josesilva",
+  "email": "josesilva33@gmail.com",
+  "password": "12345",
+  "avatar": "https://avatars.githubusercontent.com/u/124454804?v=4",
+  "background": "https://i.imgur.com/12w6KHf.png"
+}
+*/
